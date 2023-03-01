debd9444cf6bddfffbce29cd685ef37b2e7cd8c2
Sunil Kumar 8 <sunil.kumar2@publicissapient.com>
2023-03-01 17:56:33 +0000

    will remove it

--- src/test/java/com/logger/sunil/springboot/loggerdemo/MdcConsumerInterceptorTest.java
+++ src/test/java/com/logger/sunil/springboot/loggerdemo/MdcConsumerInterceptorTest.java
@@ -1,87 +1,40 @@
-package com.logger.sunil.springboot.loggerdemo;
-
-import org.apache.kafka.clients.consumer.ConsumerInterceptor;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MdcConsumerInterceptorTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MdcConsumerInterceptorTest.class);
-
-	private ConsumerInterceptor<String, String> interceptor;
-	private Map<String, String> expectedMdc;
-
-	@Before
-	public void setUp() {
-		interceptor = new MdcConsumerInterceptor<>();
-		expectedMdc = new HashMap<>();
-		expectedMdc.put("tenant", "acme");
-		expectedMdc.put("user", "jdoe");
-	}
+	private static final Logger logger = LoggerFactory.getLogger(MdcConsumerInterceptorTest.class);
 
 	@Test
-	public void testOnConsume() {
-		// Prepare test data
-		String topic = "test-topic";
-		int partition = 0;
-		List<String> messages = Arrays.asList("message1", "message2", "message3");
-		Map<TopicPartition, List<String>> recordsMap = new HashMap<>();
-		TopicPartition tp = new TopicPartition(topic, partition);
-		recordsMap.put(tp, messages);
+	public void testMdcConsumerInterceptor() {
+		// create test input data
+		Map<String, Object> recordHeaders = new HashMap<>();
+		recordHeaders.put("header1", "value1");
+		ConsumerRecord<String, String> record = new ConsumerRecord<>("test-topic", 0, 1L, "key", "value", recordHeaders);
 
-		// Set up the MDC with expected values
-		MDC.setContextMap(expectedMdc);
-
-		// Call the onConsume method of the interceptor
-		ConsumerRecords<String, String> consumerRecords = new ConsumerRecords<>(recordsMap);
-		ConsumerRecords<String, String> interceptedRecords = interceptor.onConsume(consumerRecords);
-
-		// Verify that the intercepted records have the expected MDC values
-		interceptedRecords.records(tp).forEach(record -> {
-			Map<String, String> mdc = MDC.getCopyOfContextMap();
-			LOG.info("MDC: {}", mdc);
-			assert(mdc != null);
-			assert(mdc.get("tenant").equals("acme"));
-			assert(mdc.get("user").equals("jdoe"));
-		});
-	}
-
-	@Test
-	public void testInterceptor() {
-		// create interceptor
+		// create the interceptor
 		MdcConsumerInterceptor<String, String> interceptor = new MdcConsumerInterceptor<>();
 
-		// create test record
-		ConsumerRecord<String, String> record = new ConsumerRecord<>("test-topic", 0, 0, "key", "value");
+		// create the test MDC context
+		Map<String, String> mdcContext = new HashMap<>();
+		mdcContext.put("key1", "value1");
+		mdcContext.put("key2", "value2");
 
-		// create test records
-		ConsumerRecords<String, String> records = new ConsumerRecords<>(Collections.singletonMap(
-				new TopicPartition("test-topic", 0), Collections.singletonList(record)));
+		// set the MDC context before calling the interceptor
+		MdcUtil.setMdcContext(mdcContext);
 
-		// apply interceptor to records
-		ConsumerRecords<String, String> interceptedRecords = interceptor.onConsume(records);
+		// call the interceptor
+		ConsumerRecord<String, String> interceptedRecord = interceptor.onConsume(record);
 
-		// assert that intercepted records is not null and has at least one element
-		assertNotNull(interceptedRecords);
-		assertFalse(interceptedRecords.isEmpty());
-
-		// assert that MDC context was set for intercepted records
-		for (ConsumerRecord<String, String> interceptedRecord : interceptedRecords) {
-			assertNotNull(interceptedRecord.headers().lastHeader("mdc"));
-		}
+		// assert that the MDC context has been added to the record headers
+		assertTrue(interceptedRecord.headers().lastHeader(MdcConsumerInterceptor.MDC_HEADER_KEY) != null, "MDC header not added");
 	}
-
 }
