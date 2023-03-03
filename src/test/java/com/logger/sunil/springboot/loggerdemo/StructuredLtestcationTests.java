8cce31b1e5aa55b63a0892173e623ea43514078d
Sunil Kumar 8 <sunil.kumar2@publicissapient.com>
2023-03-03 18:18:56 +0000

    will remove it

--- src/test/java/com/logger/sunil/springboot/loggerdemo/StructuredLtestcationTests.java
+++ src/test/java/com/logger/sunil/springboot/loggerdemo/StructuredLtestcationTests.java
@@ -1,16 +1,19 @@
-import org.json.*;
+import net.minidev.json.*;
 
 // Parse the JSON string into a JSONObject
 String jsonString = "{\"eventsource\":\"topicname\",\"data\":\"{\\\"orgid\\\":\\\"test\\\",\\\"data\\\":{\\\"relationship\\\":{\\\"payment\\\":{\\\"data\\\":[{\\\"id\\\":\\\"123\\\"}]}}}}\"}";
-		JSONObject json = new JSONObject(jsonString);
+		JSONObject json = (JSONObject) JSONValue.parse(jsonString);
 
 		// Get the "data" object
-		JSONObject data = json.getJSONObject("data");
+		JSONObject data = (JSONObject) json.get("data");
 
 		// Get the "data" object inside the "relationship" object
-		JSONObject relationshipData = data.getJSONObject("data").getJSONObject("relationship").getJSONObject("payment").getJSONArray("data").getJSONObject(0);
+		JSONObject relationshipData = (JSONObject) data.get("data");
+		relationshipData = (JSONObject) relationshipData.get("relationships");
+		relationshipData = (JSONObject) relationshipData.get("payment");
+		relationshipData = ((JSONArray) relationshipData.get("data")).getJSONObject(0);
 
 		// Get the value of the "id" field
-		String id = relationshipData.getString("id");
+		String id = (String) relationshipData.get("id");
 
 		System.out.println(id); // Output: 123
