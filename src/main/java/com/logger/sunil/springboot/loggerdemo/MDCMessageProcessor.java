f9102a42dbdae233ba92f8e414c889f83a4542bc
Sunil Kumar 8 <sunil.kumar2@publicissapient.com>
2023-03-14 08:38:06 +0000

    will remove it

--- src/main/java/com/logger/sunil/springboot/loggerdemo/MDCMessageProcessor.java
+++ src/main/java/com/logger/sunil/springboot/loggerdemo/MDCMessageProcessor.java
@@ -3,38 +3,38 @@ package com.logger.sunil.springboot.loggerdemo;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class MDCMessageProcessor {
-    private Map<Pattern, TopicHandler> handlers = new HashMap<>();
+
     private TopicHandler defaultHandler = new DefaultTopicHandler();
+    Map<Pattern, TopicHandler> handlers =Utils.registerHandlers();
     public void setDefaultHandler(DefaultTopicHandler handler) {
         defaultHandler = handler;
     }
-    public void registerHandler(String topicRegex, TopicHandler handler) {
-        Pattern pattern = Pattern.compile(topicRegex);
-        handlers.put(pattern, handler);
-    }
-    public void processMessage(String topicName, String message) {
-        boolean handled = false;
-        for (Pattern pattern : handlers.keySet()) {
-            if (pattern.matcher(topicName).matches()) {
-                TopicHandler handler = handlers.get(pattern);
-                handler.handle(topicName, message);
-                handled = true;
-            }
-        }
-        if (!handled && defaultHandler != null) {
-            defaultHandler.handle(topicName, message);
-        }
 
-    }
+public void processMessage(String topicName, String message) {
+    handlers.keySet().stream()
+            .flatMap(pattern -> {
+                if (pattern.matcher(topicName).matches()) {
+                    TopicHandler handler = handlers.get(pattern);
+                    handler.handle(topicName, message);
+                    return Stream.of(topicName);
+                }
+                return Stream.empty();
+            })
+            .findFirst()
+            .ifPresentOrElse(
+                    handledTopic -> {},
+                    () -> defaultHandler.handle(topicName, message)
+            );
+}
 
     public static void main(String[] args) {
         MDCMessageProcessor messageProcessor = new MDCMessageProcessor();
 
-        messageProcessor.registerHandler("^PFFC-ACS.*", new ACSTopicHandler());
-        messageProcessor.registerHandler("^PFFC-NOTIFICATION.*", new NotificationTopicHandler());
+
         messageProcessor.processMessage("PFFC-ACS", "A new sports team has been formed!");
-        messageProcessor.processMessage("Pdsadad", "The election results are in!");
+        messageProcessor.processMessage("PFFC", "The election results are in!");
     }
 }
---
+++ src/main/java/com/logger/sunil/springboot/loggerdemo/Utils.java
@@ -0,0 +1,19 @@
+package com.logger.sunil.springboot.loggerdemo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Utils {
+    static Map<Pattern, TopicHandler> handlers = new HashMap<>();
+    public static Map<Pattern, TopicHandler> registerHandlers()
+    {
+        registerHandler("^PFFC-ACS.*", new ACSTopicHandler());
+        registerHandler(".*NOTIFICATION.*", new NotificationTopicHandler());
+        return handlers;
+    }
+    public  static void registerHandler(String topicRegex, TopicHandler handler) {
+        Pattern pattern = Pattern.compile(topicRegex);
+        handlers.put(pattern, handler);
+    }
+}
